07d08a7b77cf183ceab7d85a8d1d9e101e044830
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-08-31 00:53:47 +0300

    Add Catalog.API microservice

---
+++ market-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+
+app.MapCarter();
+
+app.Run();
