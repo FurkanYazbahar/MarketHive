fb8efe326fcca7c452327f3aa1bd77b17720df5b
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-08-31 17:32:31 +0300

    add DeleteProductEndpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
+    }
+}
