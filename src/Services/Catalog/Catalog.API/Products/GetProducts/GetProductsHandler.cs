a11e9247abf9b73cd79466cdf024ceb9cd71e3a9
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-08-31 16:32:56 +0300

    add GetProductsEndpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            var products = await session.Query<Product>().ToListAsync(cancellationToken); 
+            
+            return new GetProductsResult(products);
+        }
+    }
+}
