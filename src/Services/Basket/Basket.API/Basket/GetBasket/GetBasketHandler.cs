5c0080b729436d8fcfae563164b8d2f67ae4f697
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-09-02 14:04:35 +0300

    add GetBasket endpoint

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserNae) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //TODO: get basket from database
+            //var basket = await _repository.GetBasket(request.UserName);
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+}
