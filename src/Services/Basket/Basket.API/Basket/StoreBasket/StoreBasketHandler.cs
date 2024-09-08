aba5e0409d05086f1f54dad63f1be1ab60663b63
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-09-08 17:45:08 +0300

    add StoreBasketEndpoints

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+    //public record StoreBasketResult(bool IsSuccess);
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            //TODO: store basket in database (use Marten upsert - if exist = update, if not
+            //TODO: update cache
+
+            return new StoreBasketResult("swn");
+        }
+    }
+}
