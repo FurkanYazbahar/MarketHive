9c8f59b29ab9cb493fceccaf676c6964aac6987f
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-09-09 00:18:20 +0300

    add exception

---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Exceptions
+{
+    public class BasketNotFoundException : NotFoundException
+    {
+        public BasketNotFoundException(string userName) : base("Basket", userName) 
+        {
+        
+        }
+    }
+}
