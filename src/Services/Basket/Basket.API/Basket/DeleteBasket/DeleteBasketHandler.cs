de4c96861cdf35e9ca70b4f576d2c8b02fd4d82e
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-09-08 18:04:07 +0300

    add DeleteBasketEndpoints

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,25 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator() 
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");        
+        }
+    }
+    public class DeleteBasketCommandHandler :
+        ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: delete basket from database and cache
+            //session.Delete<Product>(command.Id);
+
+            return new DeleteBasketResult(true);
+        }
     }
 }
