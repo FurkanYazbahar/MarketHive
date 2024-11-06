fc64ff1a1609dbd593846003bbb6734ad1566d8d
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-06 09:00:17 +0300

    add discount service

---
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.Grpc.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public sbyte ProductName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int Amount { get; set; }
+    }
+}
