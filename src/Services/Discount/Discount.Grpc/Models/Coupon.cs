e9ef1ceded659ad11d49108e156e61ce0695268b
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-06 23:58:44 +0300

    add Coupon model

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -3,7 +3,7 @@
     public class Coupon
     {
         public int Id { get; set; }
-        public sbyte ProductName { get; set; } = default!;
+        public string ProductName { get; set; } = default!;
         public string Description { get; set; } = default!;
         public int Amount { get; set; }
     }
