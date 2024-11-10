69da3c4eb5658b8a5072f53ba1a609ff2786370f
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-10 18:45:13 +0300

    add enums

---
+++ src/Services/Ordering/Ordering.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Enums
+{
+    public enum OrderStatus
+    {
+        Draft = 1,
+        Pending = 2,
+        Completed = 3,
+        Cancelled = 4
+    }
+}
