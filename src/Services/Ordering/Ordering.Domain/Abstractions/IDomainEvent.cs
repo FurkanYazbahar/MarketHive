f8f3966b714e5fc954ca7d29b38672bf3332d958
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-10 18:45:00 +0300

    add abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccuredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
