ffb0d968d57e3bc8c777b286cd53d5a7bd3d2d02
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-20 00:27:32 +0300

    Add some configurations

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Abstractions;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasConversion(
+                    CustomerId => CustomerId.Value,
+                    dbId => CustomerId.Of(dbId));
+            
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Email).HasMaxLength(255);
+
+            builder.HasIndex(c => c.Email).IsUnique();
+        }
+    }
+}
