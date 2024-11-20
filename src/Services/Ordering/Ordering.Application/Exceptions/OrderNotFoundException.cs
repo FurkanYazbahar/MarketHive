984fbb9577b9ca5d23f60d2b10a2bc53c7a36e72
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-20 22:23:14 +0300

    add Exception

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Exceptions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    public class OrderNotFoundException : NotFoundException
+    {
+        public OrderNotFoundException(Guid id) : base("Order", id)
+        {
+        }
+    }
+}
