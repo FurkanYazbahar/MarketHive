09b2005603b24a30f95d9911ccf9db0a01e2a564
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-21 00:41:20 +0300

    add event handler classes

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHander.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Microsoft.FeatureManagement;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
+{
+    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+        //(IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
+        : INotificationHandler<OrderCreatedEvent>
+    {
+        public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+        {
+
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+            //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+            //{
+            //    var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+            //    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            //}
+            return Task.CompletedTask;
+        }
+    }
+}
