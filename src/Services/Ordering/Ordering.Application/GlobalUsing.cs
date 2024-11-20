e145f08b1781695ed6c603470ebfcff005b3bfc4
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-20 22:23:43 +0300

    update globalusing file

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,2 +1,7 @@
 ﻿global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+
