a434c398dc1277c61418a7449b9515d1737991e0
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-20 22:51:10 +0300

    add delete order command

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommand(Guid OrderId)
+        : ICommand<DeleteOrderResult>;
+
+    public record DeleteOrderResult(bool IsSuccess);
+
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required");
+        }
+    }
+}
