eab414c012ee482c681a49ebcc009abb08a3a25f
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-21 00:40:18 +0300

    Add queries for CQRS in Ordering.Application layer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,23 @@
+﻿using Ordering.Application.Extentions;
+
+namespace Ordering.Application.Orders.Queries.GetOrderByName
+{
+    public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+    {
+        public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+        {
+            // get orders by name using dbContext
+            // return result
+
+            var orders = await dbContext.Orders
+                    .Include(o => o.OrderItems)
+                    .AsNoTracking()
+                    .Where(o => o.OrderName.Value.Contains(query.Name))
+                    .OrderBy(o => o.OrderName.Value)
+                    .ToListAsync(cancellationToken);
+            
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());           
+        }       
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public record GetOrdersByCustomerQuery(Guid CustomerId)
+        : IQuery<GetOrdersByCustomerResult>;
+
+    public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
+}
