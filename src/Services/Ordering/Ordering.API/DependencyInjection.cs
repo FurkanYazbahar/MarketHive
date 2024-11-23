79bde1f52f9ac6f8ca48861adb8d255e32159487
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-23 14:40:23 +0300

    add some configurations

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,18 +1,35 @@
-﻿using Carter;
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
+
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
+
             return services;
         }
         
         public static WebApplication UseApiServices(this WebApplication app)
         {
             app.MapCarter();
+
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
             return app;
         }
     }
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
      .AddApplicationServices()
      .AddInfrastructureServices(builder.Configuration)
-     .AddApiServices();
+     .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
