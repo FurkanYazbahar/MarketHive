f9a6d54bc45eed0339a8ac6b0c0761a30af98132
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-23 23:26:12 +0300

    add Aspnet Feature Managemnet Develop Feature Flag for OrderCreated Event

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHander.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHander.cs
@@ -3,21 +3,20 @@ using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain
 {
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
-        //(IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
-        : INotificationHandler<OrderCreatedEvent>
+    public class OrderCreatedEventHandler
+    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
     {
-        public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
-
             logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-            //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
-            //{
-            //    var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-            //    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
-            //}
-            return Task.CompletedTask;
+            if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+            {
+                var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+                await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            }
         }
+
     }
 }
