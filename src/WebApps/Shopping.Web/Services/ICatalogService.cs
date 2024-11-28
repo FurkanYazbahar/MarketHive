91f15d1f9a0b77fcbb1cbd6a4009f4d32f6e4c5f
FurkanYazbahar <yazbaharfurkan@gmail.com>
2024-11-28 11:56:08 +0300

    add ICatalogService

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {        
+        Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+        
+        Task<GetProductByIdResponse> GetProduct(Guid id);
+        
+        Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+    }
+}
